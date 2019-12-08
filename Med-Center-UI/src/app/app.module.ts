51248745f5ba8eb2781cc59fb120d95dc04e3b07
Dagmara Boratyn <37602117+DBoratyn@users.noreply.github.com>
2019-12-08 10:25:18 +0000

    Login in the UI
    Simple log in page that shows other components when logged in. Reverts on logout.

--- Med-Center-UI/src/app/app.module.ts
+++ Med-Center-UI/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { NavComponent } from './nav/nav.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      UserComponent
+      UserComponent,
+      NavComponent,
+      LoginComponent
    ],
    imports: [
       BrowserModule,
-      HttpClientModule
+      HttpClientModule,
+      FormsModule
    ],
    providers: [],
    bootstrap: [
---
+++ Med-Center-UI/src/app/login/login.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  model: any = {};
+
+  constructor(public authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.authService.login(this.model).subscribe(next => {
+      console.log('login success');
+    }, error => {
+      console.log('error trying to login');
+    });
+  }
+
+}
