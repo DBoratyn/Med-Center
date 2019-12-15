c32653c8e53637d8cf62af77a48b374eed017f9f
Dagmara Boratyn <37602117+DBoratyn@users.noreply.github.com>
2019-12-15 09:04:04 +0000

    revert other revert

---
+++ Med-Center-API/Dtos/UserForUpdateDto.cs
@@ -0,0 +1,19 @@
+namespace Med_Center_API.Dtos
+{
+    public class UserForUpdateDto
+    {
+        public string Username {get; set;}
+        public string Role {get; set;}
+        public string Profession {get;set;}
+        public string Pesel {get;set;}
+        public string Name {get;set;}
+        public string SecondName {get;set;}
+        public string Surname {get;set;}
+        public string DateOfBirth {get;set;}
+        public string PlaceOfBirth {get;set;}
+        public string City {get;set;}
+        public string Street {get;set;}
+        public string HouseNumber {get;set;}
+        public string ZipCode {get;set;}
+    }
+}
